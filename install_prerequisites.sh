2169916cbb260e7e1693490ee206da7136f0da0c
sminot <sam@onecodex.com>
2015-09-01 21:43:12 -0700

    Create install_prerequisites.sh

---
+++ install_prerequisites.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This script is used to install all programs needed to perform further analysis
+
+# Bowtie + Cufflinks
+
+# Samtools
+
+# BEDTools
+
+# iPython[notebook] + pandas + matplotlib
+
+# sra-toolkit
+
