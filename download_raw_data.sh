621c7346982e38a1165e1d9c55c32e81b480fefd
sminot <sam@onecodex.com>
2015-09-01 21:45:07 -0700

    Create download_raw_data.sh

---
+++ download_raw_data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Download the raw data to this machine from public repositories
+# Use FTP for the GEO datasets and sra-utilities `fastq-dump` for the SRA accessions
+
+# GSE53144
+
+# GSE41367
+
+# SRP034667
